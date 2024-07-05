163ea901f36d013f720d508eddaf804cf9210648
Thirza <thirzahmad@gmail.com>
2024-07-05 15:48:29 +0700

    feat: tailwind,login,register

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,31 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeBody from './components/HomeBody'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login'
+import Register from './components/Register'
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <Header/>
-      <Footer/>
+    <Router>
+    <div className='mx-10'>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Navbar />
+            <Header />
+            <HomeBody />
+            <section className='mt-20'>
+              <Footer />
+            </section>
+          </>
+        } />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
+  </Router>
   )
 }
 
---
+++ src/components/CardFunc.tsx
@@ -0,0 +1,9 @@
+const CardFunc = ({title}: {title: string}) => {
+    return (
+        <div className="card py-20 mx-10 items-center rounded-[6px] shadow-lg">
+        <h2 className="primary-nav title">{title}</h2>
+        </div>
+    );
+}
+
+export default CardFunc;
